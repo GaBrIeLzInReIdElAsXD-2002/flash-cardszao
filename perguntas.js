a11d7c42230bf850fdc003d93f937aa59b71f1d9
GaBrIeLzInReIdElAsXD-2002 <00001077221897sp@al.educacao.sp.gov.br>
2024-11-17 21:51:22 -0300

    commitado sa bagaça

---
+++ perguntas.js
@@ -0,0 +1,84 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O Html é uma linguagem de marcação'
+)
+
+criaCartao(
+    'Programação',
+    'Quanto é "2" + 3 em Js?',
+    '5'
+)
+
+
+
+criaCartao(
+    'Automobilismo',
+    'Qual marca venceu mais nas 24 Horas de Le Mans?',
+    'Porsche'
+)
+
+
+
+criaCartao(
+    'Astronomia',
+    'Maior Buraco Negro',
+    'TON 618'
+)
+
+
+
+
+
+criaCartao(
+    'Educação',
+    'Plataforma mais podre do novo ensino médio',
+    'Education First'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tnoevry Grvkrven, niryvab*/
